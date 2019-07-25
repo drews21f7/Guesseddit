c01125550cf14be1fa54bfb757f8a8ed0b11f187
Drew Seeholzer <drews21f7@gmail.com>
2019-07-25 10:54:22 -0600

    Organized file structure, made Model file.

---
+++ Guesseddit/Models/Reddit.swift
@@ -0,0 +1,36 @@
+//
+//  Reddit.swift
+//  Guesseddit
+//
+//  Created by Drew Seeholzer on 7/24/19.
+//  Copyright © 2019 Drew Seeholzer. All rights reserved.
+//
+
+import Foundation
+
+struct TopLevelJSON: Codable {
+    let data: [SecondLevelJSON]
+}
+
+struct SecondLevelJSON: Codable {
+    let children: [ThirdLevelJSON]
+}
+
+struct ThirdLevelJSON: Codable {
+    let data: [RedditPost]
+}
+
+struct RedditPost: Codable {
+    
+    var title: String
+    var upVotes: Int
+    var imageURLAsString: String
+    var isNSFW: Bool
+    
+    enum CodingKeys: String, CodingKey {
+        case title
+        case upVotes = "ups"
+        case imageURLAsString = "thumbnail"
+        case isNSFW = "over_18"
+    }
+}
