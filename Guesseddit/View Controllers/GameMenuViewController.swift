0ce0c290982a795977fd071f3a0d4a893573138f
Drew Seeholzer <drews21f7@gmail.com>
2019-08-01 10:19:44 -0600

    Added the rest of the subreddits to be able to be selected, fixed game crashing from trying to start game without a subreddit selected.

--- Guesseddit/View Controllers/GameMenuViewController.swift
+++ Guesseddit/View Controllers/GameMenuViewController.swift
@@ -16,85 +16,124 @@ class GameMenuViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        subRedditChoiceLabel.text = ""
         
     }
     
     //MARK: - Subreddit Buttons
     @IBAction func askRedditButtonTapped(_ sender: Any) {
-    }
-    @IBAction func todayILearnedButtonTapped(_ sender: Any) {
-    }
-    @IBAction func worldNewsButtonTapped(_ sender: Any) {
-    }
-    @IBAction func scienceButtonTapped(_ sender: Any) {
-    }
-    @IBAction func picsButtonTapped(_ sender: Any) {
-        if RedditPostController.sharedInstance.redditPosts.count == 0 {
-            RedditPostController.sharedInstance.fetchSubRedditPosts(subReddit: "pics") { (pics) in
-                if let pics = pics {
-                    RedditPostController.sharedInstance.redditPosts = pics
-                    self.subRedditChosen = true
-                    DispatchQueue.main.async {
-                        self.subRedditChoiceLabel.text = "Pics"
-                    }
+        
+        RedditPostController.sharedInstance.fetchSubRedditPosts(subReddit: "askreddit") { (subReddit) in
+            if let subReddit = subReddit {
+                RedditPostController.sharedInstance.redditPosts = subReddit
+                self.subRedditChosen = true
+                DispatchQueue.main.async {
+                    self.subRedditChoiceLabel.text = "Ask Reddit"
                 }
             }
-        } else {
-            RedditPostController.sharedInstance.redditPosts = []
-            RedditPostController.sharedInstance.fetchSubRedditPosts(subReddit: "pics") { (pics) in
-                if let pics = pics {
-                    RedditPostController.sharedInstance.redditPosts = pics
-                    self.subRedditChosen = true
-                    DispatchQueue.main.async {
-                        self.subRedditChoiceLabel.text = "Pics"
-                        
-                    }
+        }
+    }
+    @IBAction func todayILearnedButtonTapped(_ sender: Any) {
+        
+        RedditPostController.sharedInstance.fetchSubRedditPosts(subReddit: "todayilearned") { (subReddit) in
+            if let subReddit = subReddit {
+                RedditPostController.sharedInstance.redditPosts = subReddit
+                self.subRedditChosen = true
+                DispatchQueue.main.async {
+                    self.subRedditChoiceLabel.text = "Today I Learned"
+                }
+            }
+        }
+    }
+    @IBAction func worldNewsButtonTapped(_ sender: Any) {
+        
+        RedditPostController.sharedInstance.fetchSubRedditPosts(subReddit: "worldnews") { (subReddit) in
+            if let subReddit = subReddit {
+                RedditPostController.sharedInstance.redditPosts = subReddit
+                self.subRedditChosen = true
+                DispatchQueue.main.async {
+                    self.subRedditChoiceLabel.text = "World News"
+                }
+            }
+        }
+    }
+    @IBAction func scienceButtonTapped(_ sender: Any) {
+        
+        RedditPostController.sharedInstance.fetchSubRedditPosts(subReddit: "science") { (subReddit) in
+            if let subReddit = subReddit {
+                RedditPostController.sharedInstance.redditPosts = subReddit
+                self.subRedditChosen = true
+                DispatchQueue.main.async {
+                    self.subRedditChoiceLabel.text = "Science"
+                }
+            }
+        }
+    }
+    @IBAction func picsButtonTapped(_ sender: Any) {
+
+        RedditPostController.sharedInstance.fetchSubRedditPosts(subReddit: "pics") { (subReddit) in
+            if let subReddit = subReddit {
+                RedditPostController.sharedInstance.redditPosts = subReddit
+                self.subRedditChosen = true
+                DispatchQueue.main.async {
+                    self.subRedditChoiceLabel.text = "Pics"
                 }
             }
         }
     }
     @IBAction func gamingButtonTapped(_ sender: Any) {
-        if RedditPostController.sharedInstance.redditPosts.count == 0 {
-            RedditPostController.sharedInstance.fetchSubRedditPosts(subReddit: "gaming") { (gaming) in
-                if let gaming = gaming {
-                    RedditPostController.sharedInstance.redditPosts = gaming
+       // if RedditPostController.sharedInstance.redditPosts.count == 0 {
+            RedditPostController.sharedInstance.fetchSubRedditPosts(subReddit: "gaming") { (subReddit) in
+                if let subReddit = subReddit {
+                    RedditPostController.sharedInstance.redditPosts = subReddit
                     self.subRedditChosen = true
                     DispatchQueue.main.async {
                         self.subRedditChoiceLabel.text = "Gaming"
                     }
                 }
             }
-        } else {
-            RedditPostController.sharedInstance.redditPosts = []
-            RedditPostController.sharedInstance.fetchSubRedditPosts(subReddit: "gaming") { (gaming) in
-                if let gaming = gaming {
-                    RedditPostController.sharedInstance.redditPosts = gaming
-                    self.subRedditChosen = true
-                    DispatchQueue.main.async {
-                        self.subRedditChoiceLabel.text = "Gaming"
-                        
-                    }
-                }
-            }
-        }
+      //  } //else {
+//            RedditPostController.sharedInstance.redditPosts = []
+//            RedditPostController.sharedInstance.fetchSubRedditPosts(subReddit: "gaming") { (gaming) in
+//                if let gaming = gaming {
+//                    RedditPostController.sharedInstance.redditPosts = gaming
+//                    self.subRedditChosen = true
+//                    DispatchQueue.main.async {
+//                        self.subRedditChoiceLabel.text = "Gaming"
+//
+//                    }
+//                }
+//            }
+//        }
     }
     
     
     // MARK: - Navigation
-    
-    //View Changing Buttons
-    @IBAction func playGameButtonTapped(_ sender: Any) {
-        if subRedditChosen == true {
-            //performSegue(withIdentifier: "toGameSegue", sender: subRedditChosen)
-        }// TODO: - Make notification when false
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        if identifier == "toGameSegue" {
+            if subRedditChosen == true {
+                return true
+            }else{
+                //present an alert controller
+                let alertController = UIAlertController.init(title: "No Subreddit selected", message: "You must first select a subreddit", preferredStyle: .alert)
+                let ok = UIAlertAction.init(title: "Ok", style: .cancel)
+                alertController.addAction(ok)
+                present(alertController, animated: true)
+                return false 
+            }
+        }
+        return true
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toGameSegue" {
+            
+        }
+        if segue.identifier == "toGameLeaderboard" {
+            
+        }
+    }
+
     @IBAction func viewLeaderboardButtonTapped(_ sender: Any) {
     }
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-   // override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
-        
-   // }
-
 }
