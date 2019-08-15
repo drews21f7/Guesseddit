5dc1431e3e5286f8196764b4fff4a97893c47580
Drew Seeholzer <drews21f7@gmail.com>
2019-08-15 12:27:13 -0600

    Now presents an alert when username is invalid on sign up view.

--- Guesseddit/View Controllers/SignInViewController.swift
+++ Guesseddit/View Controllers/SignInViewController.swift
@@ -41,6 +41,9 @@ class SignInViewController: UIViewController, UITextFieldDelegate {
                 if user != nil {
                     UserController.sharedInstance.currentUser = user
                     self.presentGameMenuView()
+                } else {
+                    self.resignFirstResponder()
+                    self.invalidUserNameAlert()
                 }
             }
 
@@ -66,5 +69,14 @@ extension SignInViewController {
             let gameMenuViewController = storyboard.instantiateViewController(withIdentifier: "GameMenuNavVC")
             self.present(gameMenuViewController, animated: true)
         }
+    }
+    
+    func invalidUserNameAlert() {
+        let alertController = UIAlertController(title: "Invalid User name", message: "Make sure you're signed into your apple account on your phone and have a name entered", preferredStyle: .alert)
+        let confirmAction = UIAlertAction(title: "Ok", style: .cancel)
+        
+        alertController.addAction(confirmAction)
+        present(alertController, animated: true)
+        
     }
 }
